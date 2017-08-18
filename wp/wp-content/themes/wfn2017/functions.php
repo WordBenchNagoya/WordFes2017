561d88e11043b86a34ff37bf7cb0e191f73c51ab
泉　陽二 <teburashika@gmail.com>
2017-08-18 16:00:59 +0900

    ナビゲーション機能の調整

--- wp/wp-content/themes/wfn2017/functions.php
+++ wp/wp-content/themes/wfn2017/functions.php
@@ -156,6 +156,8 @@ function wordfes2017_scripts() {
 	wp_enqueue_script( 'wordfes2017-scripts', get_template_directory_uri() . '/js/scripts.js', array(), null, true );
 	wp_enqueue_script( 'wordfes2017-slider', get_template_directory_uri() . '/js/jquery.bxslider.min.js', array(), null, true );
 
+	wp_enqueue_script( 'wordfes2017-navigation', get_template_directory_uri() . '/js/navigation.js', array(), null, true );
+
 	// bootstrap
 	wp_enqueue_script( 'wordfes2017-bootstrap', get_template_directory_uri() . '/bootstrap/js/bootstrap.min.js', array( 'jquery' ), null, true );
 
