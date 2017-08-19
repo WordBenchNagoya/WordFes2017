1163243582970dba77fa1f2987c51b9c5d850b99
ryoraspp <ryraspp@gmail.com>
2017-08-19 15:24:42 +0900

    サイドバーのnavを削除

--- wp/wp-content/themes/wfn2017/sidebar.php
+++ wp/wp-content/themes/wfn2017/sidebar.php
@@ -1,21 +1,7 @@
 		<aside class="sidebar col-sm-4 col-xs-12">
 			
 			<h2>開催概要</h2>
-			
-			<nav id="side-navigation">
 				
-				<div class="navigation-menu">
-					<?php wp_nav_menu( array(
-						'theme_location' => 'secondary',
-						'menu_class'     => 'nav-menu',
-						'menu_id'        => 'secondary-menu',
-						'depth'          => 2,
-					) );
-					?>
-				</div>
-				
-			</nav><!-- #site-navigation -->
-			
 			<div class="banner-container">
 				<?php dynamic_sidebar('banner-area'); ?>
 			</div>
