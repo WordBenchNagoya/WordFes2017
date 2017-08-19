04ba574982d0f0330d7e926d6cfa0fd6b29cc89f
Katz Ueno <katz@concrete5.co.jp>
2017-08-20 08:35:38 +0900

    Site Title 修正

--- wp/wp-content/themes/wfn2017/header.php
+++ wp/wp-content/themes/wfn2017/header.php
@@ -21,11 +21,11 @@
 <?php
 if ( is_front_page() ) {
 	
-	$title = 'WordFes Nagoya 2017 | WordPress の森に集おう！';
+	$title = 'WordFes Nagoya 2017 これから 〜未来へ繋ぐ〜';
 	
 } else {
 	
-	$title = get_the_title() . ' | WordFes Nagoya 2017'; 
+	$title = get_the_title() . ' | WordFes Nagoya 2017 これから 〜未来へ繋ぐ〜'; 
 	
 }
 ?>
