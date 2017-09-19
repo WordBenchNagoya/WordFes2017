0e3e8e0ccf83a3f36ce04a65999e53d90acaea31
ryoraspp <ryoraspp@gmail.com>
2017-09-19 20:40:34 +0900

    slider sample 6

--- wp/wp-content/themes/wfn2017/header.php
+++ wp/wp-content/themes/wfn2017/header.php
@@ -117,8 +117,7 @@ if ( is_front_page() ) {
 
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.bxslider.min.js"></script>
 <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/jquery.bxslider.css">
-<script type="text/javascript">
-$(function(){
+jQuery(function($) {
 $('.bxslider').bxSlider({
 	auto: true,
 	slideWidth: 486,
