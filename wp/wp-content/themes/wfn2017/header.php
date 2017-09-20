29bff90e48466321fbac0cc700a44ad32b970f94
ryoraspp <ryoraspp@gmail.com>
2017-09-21 06:04:05 +0900

    slider sample 17

--- wp/wp-content/themes/wfn2017/header.php
+++ wp/wp-content/themes/wfn2017/header.php
@@ -117,6 +117,28 @@ if ( is_front_page() ) {
 スライダーサンプル
 
 <script type="text/javascript">
+jQuery(document).ready(function($){
+$('.bxslider').bxSlider({
+	slideWidth: 486,
+	slideHeight: 323,
+	minSlides: 2,
+	mode: horizontal,
+	infiniteLoop: true,
+	speed: 1000,
+	startSlide: 0,
+	randomStart: true,
+	slideMargin: 0,
+	ticker: true,
+	captions: true,
+	pager: true,
+`	controls: false,
+	auto: true,
+	moveSlides: 2,
+	touchEnabled: true,
+	oneToOneTouch: true,
+	preventDefaultSwipeX: true
+});
+})(jQuery);
 </script>
 
 <ul class="bxslider">
