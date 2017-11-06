3f9b25731f29a79affdf69bcb4a2a195dae4fefa
ryoraspp <ryoraspp@gmail.com>
2017-11-07 01:39:58 +0900

    Add session slide oEmbed var

--- wp/wp-content/themes/wfn2017/parts/content-session.php
+++ wp/wp-content/themes/wfn2017/parts/content-session.php
@@ -29,7 +29,12 @@ $wordpressorg =get_field('session_wordpress_org');
 					<p class="content">
 						<?php echo wp_kses_post( pdc_get_ret2br_text( $contents ) ); ?>
 					</p>
-				
+				<?php if( get_field('session_slide_oembed') ){ ?>
+				<div class="VideoWrapper">
+   				 <?php echo $embed_code = wp_oembed_get( get_field('session_slide_oembed''), array( 'max-width' => '100%', 'height' => 'auto' ) ); ?>
+				</div>
+				<?php } ?>
+				<br>
 				<?php if( get_field('session_youtube') ){ ?>
 				<div class="VideoWrapper">
    				 <?php echo $embed_code = wp_oembed_get( get_field('session_youtube'), array( 'max-width' => '100%', 'height' => 'auto' ) ); ?>
